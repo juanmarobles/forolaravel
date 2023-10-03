1195919c4022f1fe690a4cfef141943ca9c027ca
juanmarobles <roblesc4rp@gmail.com>
2023-10-03 14:14:32 -0300

    up

---
+++ resources/views/livewire/show-threads.blade.php
@@ -0,0 +1,22 @@
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-10 py-12">
+    <div class="w-64">
+        <a href="" class="block w-full py-4 mb-10  text-white bg-blue-800 hover:bg-blue-500 bg-contain bg-center rounded-md text-center font-bold">Preguntar</a>
+    
+    <div class="w-full">
+        <ul>
+            <li class="mb-2">
+                <a href="" class="py-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-sm capitalize">
+                <span class="w-2 h-2 rounded-full" style="background-color: #00aced;"></span>    
+                Ejemplo
+                </a>
+            </li>
+            <li>
+                <a href="" class="py-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-sm capitalize">
+                <span class="w-2 h-2 rounded-full" style="background-color: #000;"></span>    
+                Todos los resultados
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Livewire\ShowThreads;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', ShowThreads::class)
+->middleware(['auth'])
+->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
